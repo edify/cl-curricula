d2f694ecba8deeed186fb54efa3fb1b2317891b5
diugalde <dua.0695@gmail.com>
2016-09-10 01:15:07 -0600

    Add authentication and error handling

--- main.js
+++ main.js
@@ -2,3 +2,22 @@
  * Created by diugalde on 08/09/16.
  */
 
+const app = require('./lib/app');
+const config = require('./lib/config');
+const log = require('./lib/utils/log');
+
+
+(function main() {
+    log.info('Starting cl-curricula micro service...');
+
+    var server = app.createServer({
+        log: log,
+        apiURL: config.server.apiURL,
+        audit: true,
+        testing: false
+    });
+
+    server.listen(config.server.port, function onListening() {
+        log.info('Server listening at %s', 'localhost:' + config.server.port);
+    });
+})();
